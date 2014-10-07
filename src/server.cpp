897f5e18c2fb4a25dae28a30d80ba82ea3b4e172
Séverin Lemaignan <severin.lemaignan@epfl.ch>
2014-10-07 22:02:35 +0200

    Test server, receiving the web interface JSON
    Relies on cpp-netlib + jsoncpp

---
+++ src/server.cpp
@@ -0,0 +1,60 @@
+#include <boost/network/uri.hpp>
+#include <boost/network/protocol/http/server.hpp>
+#include <string>
+#include <iostream>
+
+#include <json/json.h>
+
+using namespace std;
+
+namespace http = boost::network::http;
+namespace uri = boost::network::uri;
+
+Json::Value root; // will contains the root value after parsing.
+Json::Reader reader;
+
+struct handler;
+typedef http::server<handler> http_server;
+
+void replaceAll(std::string& str, const std::string& from, const std::string& to) {
+
+    size_t start_pos = 0;
+    while((start_pos = str.find(from, start_pos)) != std::string::npos) {
+        str.replace(start_pos, from.length(), to);
+        start_pos += to.length(); // In case 'to' contains 'from', like replacing 'x' with 'yx'
+    }
+}
+
+struct handler {
+    void operator() (http_server::request const &request,
+                     http_server::response &response) {
+        string ip = source(request);
+        string path = request.destination;
+        string query = path.substr(path.find("content=") + 8);
+        replaceAll(query,"%22","\"");
+        replaceAll(query,"%20"," ");
+        bool parsingSuccessful = reader.parse( query, root );
+        if (!parsingSuccessful) {
+            cerr << "Invalid command: " << query << endl;
+            return;
+        }
+
+        cout << root;
+
+        response = http_server::response::stock_reply(
+            http_server::response::ok, "Hello, world!");
+    }
+
+    void log(http_server::string_type const &info) {
+        std::cerr << "ERROR: " << info << '\n';
+    }
+};
+
+int main(int arg, char * argv[]) {
+    handler handler_;
+    http_server::options options(handler_);
+    http_server server_(
+        options.address("0.0.0.0")
+               .port("8000"));
+    server_.run();
+}
