247742822abba1b0987d26cb771d9e3cc886c3c9
Séverin Lemaignan <severin.lemaignan@epfl.ch>
2014-11-17 13:11:24 +0100

    Improved a little bit the hough detection test tool

--- tools/circles.py
+++ tools/circles.py
@@ -3,22 +3,27 @@ import numpy as np
 import sys
 
 img = cv2.imread(sys.argv[1],0)
-img = cv2.medianBlur(img,5)
+img = cv2.resize(img, (320,240))
+#img = cv2.medianBlur(img,5)
 cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
 
-circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,40,
-                           param1=50,param2=30,minRadius=5,maxRadius=40)
+circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,
+                           param1=40,param2=25,minRadius=5,maxRadius=20)
 
 if circles == None:
     print ("No circles!")
-    sys.exit(0)
-circles = np.uint16(np.around(circles))
-for i in circles[0,:]:
-    # draw the outer circle
-    cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
-    # draw the center of the circle
-    cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+    cv2.imshow('detected circles',cimg)
+    cv2.waitKey(0)
+else:
+    print("Found %s circles" % len(circles[0,:]))
+    circles = np.uint16(np.around(circles))
+    for i in circles[0,:]:
+        # draw the outer circle
+        cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+        # draw the center of the circle
+        cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
 
-cv2.imshow('detected circles',cimg)
-cv2.waitKey(0)
+        cv2.imshow('detected circles',cimg)
+        cv2.waitKey(0)
+
 cv2.destroyAllWindows()
