6611aaa2925bbf3a20d9760e6176fc987b55c98c
Séverin Lemaignan <severin.lemaignan@epfl.ch>
2014-12-03 22:31:18 +0100

    Improved version of the python prototype for tag detection

--- tools/circles.py
+++ tools/circles.py
@@ -1,29 +1,44 @@
+#! /usr/bin/env python
+
 import cv2
 import numpy as np
 import sys
 
-img = cv2.imread(sys.argv[1],0)
-img = cv2.resize(img, (320,240))
-#img = cv2.medianBlur(img,5)
-cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+from undistort import undistort
 
-circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,
-                           param1=40,param2=25,minRadius=5,maxRadius=20)
+total = 0
+for f in sys.argv[1:]:
+    img = cv2.imread(f,0)
+    img = undistort("../share/table_calib_640x480.yml", img)
+    img = cv2.resize(img, (320,240))
+    #img = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
+    #                            cv2.THRESH_BINARY,15,2)
 
-if circles == None:
-    print ("No circles!")
-    cv2.imshow('detected circles',cimg)
-    cv2.waitKey(0)
-else:
-    print("Found %s circles" % len(circles[0,:]))
-    circles = np.uint16(np.around(circles))
-    for i in circles[0,:]:
-        # draw the outer circle
-        cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
-        # draw the center of the circle
-        cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+
+    #img = cv2.medianBlur(img,5)
+    cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+
+    
+    circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,
+                            param1=30,param2=20,minRadius=5,maxRadius=20)
+
+    if circles == None:
+        #print (f + ": No circles!")
+        #cv2.imshow('detected circles',cimg)
+        #cv2.waitKey(0)
+        pass
+    else:
+        total += len(circles[0,:])
+        #print(f + ": Found %s circles" % len(circles[0,:]))
+        circles = np.uint16(np.around(circles))
+        for i in circles[0,:]:
+            # draw the outer circle
+            cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+            # draw the center of the circle
+            cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
 
         cv2.imshow('detected circles',cimg)
         cv2.waitKey(0)
 
 cv2.destroyAllWindows()
+print("Found in total %s circles" % total)
--- tools/undistort.py
+++ tools/undistort.py
@@ -4,31 +4,39 @@ import numpy as np
 import cv2
 import yaml
 
-if not sys.argv[1].endswith("yml"):
-    print("Usage: undistort.py calib.yml images...")
-    sys.exit(1)
+def undistort(calib, img):
+    
+    calib = open(calib,'r')
+    calibdata = yaml.load(calib)
 
-calib = open(sys.argv[1],'r')
-calibdata = yaml.load(calib)
+    K = np.array(calibdata["camera_matrix"]["data"]).reshape((3,3))
+    d = np.array(calibdata["distortion_coefficients"]["data"])
 
-K = np.array(calibdata["camera_matrix"]["data"]).reshape((3,3))
-d = np.array(calibdata["distortion_coefficients"]["data"])
+    #print("Camera matrix:")
+    #print(K)
+    #print("Distortion coefficients:")
+    #print(d)
 
-print("Camera matrix:")
-print(K)
-print("Distortion coefficients:")
-print(d)
-
-for f in sys.argv[2:]:
-    # read one of your images
-    img = cv2.imread(f)
     h, w = img.shape[:2]
 
     # undistort
     newcamera, roi = cv2.getOptimalNewCameraMatrix(K, d, (w,h), 0) 
-    newimg = cv2.undistort(img, K, d, None, newcamera)
+    return cv2.undistort(img, K, d, None, newcamera)
 
-    name = os.path.basename(f).split(".")[0]
-    cv2.imwrite("%s_undistorted.jpg" % name, newimg)
 
-print("Done undistorting %s images." % len(sys.argv[2:]))
+if __name__ == "__main__":
+
+    if not sys.argv[1].endswith("yml"):
+        print("Usage: undistort.py calib.yml images...")
+        sys.exit(1)
+
+    for f in sys.argv[2:]:
+        # read one of your images
+        img = cv2.imread(f)
+
+        newimg = undistort(sys.argv[1], img)
+
+        name = os.path.basename(f).split(".")[0]
+        cv2.imwrite("%s_undistorted.jpg" % name, newimg)
+
+    print("Done undistorting %s images." % len(sys.argv[2:]))
