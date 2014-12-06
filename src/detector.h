5dc2e4cb48ae98c1b31d3aebd70f82e51e9ee5db
Séverin Lemaignan <severin.lemaignan@epfl.ch>
2014-12-06 14:20:12 +0100

    Properly structured tag detector code

---
+++ src/detector.h
@@ -0,0 +1,44 @@
+#ifndef _DETECTOR_H
+#define _DETECTOR_H
+
+#include <opencv2/core/core.hpp>
+#include <map>
+#include <string>
+
+class Detector {
+
+public:
+    cv::Size readCalibration(const std::string &filename);
+
+    void prepare(cv::InputArray rawimage, cv::OutputArray undistorted_img, cv::OutputArray small_img);
+
+    void detect_circles(cv::InputArray img, cv::OutputArray circles);
+
+
+    void unfold_circle(cv::InputArray circle);
+
+    void decode_marker(cv::InputArray img, cv::Vec3f circle);
+
+    void find_markers(cv::InputArray img, const std::string& calibration);
+
+private:
+
+    // defines the ID and patterns of the various markers.
+    // ' ' for white, '-' for black
+    const std::map<char, std::string> markers {
+        {1, "     -----     -----"},
+        {2, "            --------"},
+        {3, "-------      -------"}
+        };
+
+    // how many samples to take on a circle (ie how many radii) to recognize
+    // the tag. Higher values increase the accuracy of orientation
+    const int NB_RADII = 32;
+
+    cv::Rect circle2rect(cv::Vec3f circle, double scaling = 1.0);
+    cv::Mat distCoeffs, cameraMatrix;
+
+
+};
+
+#endif
