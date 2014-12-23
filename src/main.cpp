37775e806cb59d3bb3a84df3456ac47b0c7dac93
Séverin Lemaignan <severin.lemaignan@epfl.ch>
2014-12-23 18:54:32 +0100

    Use the external switch for a 'soft' poweroff
    Not tested yet

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,28 @@
 #include "http_server/server.hpp"
 #include "handler.h"
 #include "table.h"
+#include "gpio.h"
 
 using namespace std;
 
 static const double MAIN_LOOP_FPS=60;
 static const chrono::milliseconds main_loop_duration{(int) (1000/MAIN_LOOP_FPS)};
 
+static const int POWERSWITCH_GPIO=27;
 
 bool running = true;
 
 
 int main(int arg, char * argv[]) {
+
+    // 'active' toggles when the power switch is pressed (ie, 'soft' poweroff)
+    bool active = true;
+
+    /*
+     * Enable and configure  GPIO pin 27 (ie, GPIO 2 on the board)
+     */
+    if (-1 == GPIOExport(POWERSWITCH_GPIO)) return(1);
+    if (-1 == GPIODirection(POWERSWITCH_GPIO, IN)) return(2);
 
     auto table = make_shared<Table>();
     auto src1 = make_shared<LightSource>(1);
@@ -52,23 +63,45 @@ int main(int arg, char * argv[]) {
       });
 
 
+    auto last_mode = PLAIN;
+
     auto start = chrono::high_resolution_clock::now();
     auto intermediate = start, end = start;
     chrono::milliseconds dt{0};
 
+    
     while (running) {
 
-        s.poll();
+        // active = active XOR switch_pressed
+        active = (active != (GPIORead(POWERSWITCH_GPIO) == 1));
 
-        table->step(dt);
+        if (active) {
+            // if the table was previously inactive, reset the mode to the
+            // last one.
+            if (table->mode == CLOSING) table->mode = last_mode;
 
-        intermediate = chrono::high_resolution_clock::now();
-        
-        this_thread::sleep_for(main_loop_duration - (intermediate - start));
-        end = chrono::high_resolution_clock::now();
-        dt = chrono::duration_cast<chrono::milliseconds>(end-start);
+            s.poll();
 
-        start = chrono::high_resolution_clock::now();
+            table->step(dt);
+
+            intermediate = chrono::high_resolution_clock::now();
+
+            this_thread::sleep_for(main_loop_duration - (intermediate - start));
+            end = chrono::high_resolution_clock::now();
+            dt = chrono::duration_cast<chrono::milliseconds>(end-start);
+
+            start = chrono::high_resolution_clock::now();
+        }
+        else {
+            if (table->mode != CLOSING) {
+                cout << "Going to 'soft' sleep" << endl;
+                last_mode = table->mode;
+                table->mode = CLOSING;
+                table->step(dt); // blocks until LEDs fade out
+            }
+
+            this_thread::sleep_for(main_loop_duration);
+        }
     }
 
     table->mode = CLOSING;
