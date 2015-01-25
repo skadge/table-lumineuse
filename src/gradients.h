0d3e5935e9dd1687751cfdcf3e402c34ee0ce086
Séverin Lemaignan <severin@guakamole.org>
2015-01-25 21:16:11 +0000

    Added new gradients (fire and ocean)

--- src/gradients.h
+++ src/gradients.h
@@ -9,3 +9,17 @@ const static Gradient FOREST_GRADIENT({
         {1.f, {0, 36, 0}}
     });
 
+const static Gradient FIRE_GRADIENT({
+        {0.f, {255, 48, 25}},
+        {0.43f, {237, 80, 23}},
+        {0.53f, {249, 201, 24}},
+        {1.f, {207, 4, 4}}
+    });
+
+const static Gradient OCEAN_GRADIENT({
+        {0.f, {44, 83, 158}},
+        {0.25f, {7, 13, 28}},
+        {0.45f, {53, 198, 214}},
+        {1.f, {44, 83, 158}}
+    });
+
