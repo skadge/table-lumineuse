9f6085bc9eabf0d5a0abacaf6e21da8cb5bfa8cc
Severin Lemaignan <severin@guakamole.org>
2019-12-27 22:13:46 +0000

    Fixed the memory corruption issues with LPD8806

--- src/light/lpd8806led.c
+++ src/light/lpd8806led.c
@@ -24,7 +24,7 @@ static uint8_t gamma_table_blue[256];
 
 int lpd8806_init(lpd8806_buffer *buf, int leds) {
   buf->leds = leds;
-  buf->size = (leds+3)*sizeof(lpd8806_color);
+  buf->size = (leds+5)*sizeof(lpd8806_color);
   buf->buffer = (lpd8806_color*)malloc(buf->size);
   if(buf->buffer==NULL) {
     return -1;
